f5d2c0487ed9743abeb13ebc9c09bc2cd29b29e1
Rong Shen Ng <ngrongshen2005@gmail.com>
2026-02-22 11:56:23 -0800

    Flashlight code

--- flashlight.cs
+++ flashlight.cs
@@ -1,1 +1,35 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
 
+public class SimpleFlashlight : UdonSharpBehaviour
+{
+    [Header("Assign the Light component here")]
+    public Light flashlightLight;
+
+    [Header("Start ON or OFF")]
+    public bool startOn = false;
+
+    private bool isOn;
+
+    void Start()
+    {
+        isOn = startOn;
+        UpdateLightState();
+    }
+
+    public override void Interact()
+    {
+        isOn = !isOn;
+        UpdateLightState();
+    }
+
+    private void UpdateLightState()
+    {
+        if (flashlightLight != null)
+        {
+            flashlightLight.enabled = isOn;
+        }
+    }
+}
